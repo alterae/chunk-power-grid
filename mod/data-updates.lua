cb1d29f5b7cf649a1a7a036cab723bbfc083c02a
Michelle S <alterae@protonmail.com>
2024-02-15 10:07:00 -0700

    Add type annotations

--- mod/data-updates.lua
+++ mod/data-updates.lua
@@ -1,8 +1,12 @@
 local ns = require "lib.namespace"
 
-local big_pole_reach = settings.startup[ns.settings.startup "big-electric-pole:maximum-wire-distance"]
-    .value
-local substation_area = settings.startup[ns.settings.startup "substation:supply-area-distance"].value
+local wire_distance = settings
+    .startup[ns.settings.startup "big-electric-pole:maximum-wire-distance"]
+    .value --[[@as number]]
 
-data.raw["electric-pole"]["big-electric-pole"].maximum_wire_distance = big_pole_reach
-data.raw["electric-pole"]["substation"].supply_area_distance = substation_area
+local supply_area = settings
+    .startup[ns.settings.startup "substation:supply-area-distance"]
+    .value --[[@as number]]
+
+data.raw["electric-pole"]["big-electric-pole"].maximum_wire_distance = wire_distance
+data.raw["electric-pole"]["substation"].supply_area_distance = supply_area
