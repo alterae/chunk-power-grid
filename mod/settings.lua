c5e1b63250232149cbf2536ec24ddafbf5adc069
Michelle S <alterae@protonmail.com>
2024-02-15 10:57:02 -0700

    Add type annotations

--- mod/settings.lua
+++ mod/settings.lua
@@ -1,21 +1,28 @@
+---@diagnostic disable: missing-fields
+
 local ns = require "lib.namespace"
 
 data:extend {
-    -- Whether to allow a little wiggle room for big power poles, beyond 32
-    -- tiles (to allow for slight adjustments).
     {
         name = ns.settings.startup "big-electric-pole:maximum-wire-distance",
         type = "double-setting",
-        default_value = 32.25,              -- Allows a little leeway for non-straight lines
-        allowed_values = { 30, 32, 32.25 }, -- 30 = vanilla, 32 = 1 chunk
+        default_value = 32.25,
+        allowed_values = {
+            30,    -- Vanilla
+            32,    -- One chunk
+            32.25, -- One chunk + wiggle room
+        },
         setting_type = "startup",
     },
 
     {
         name = ns.settings.startup "substation:supply-area-distance",
         type = "double-setting",
-        default_value = 9,         -- Vanilla
-        allowed_values = { 8, 9 }, -- 8 = quarter-chunk area
+        default_value = 9,
+        allowed_values = {
+            8, -- 16x16 supply area (1/4 chunk)
+            9, -- Vanilla
+        },
         setting_type = "startup",
     },
-}
+} --[[ @as LuaModSettingPrototype[] ]]
