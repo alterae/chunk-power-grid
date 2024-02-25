7584934328d790bbab12d394f9d9804f337dc993
Michelle S <alterae@protonmail.com>
2024-02-24 17:18:27 -0700

    Check that relevant electric pole prototypes actually exist before modifying them.

--- src/data-updates.ts
+++ src/data-updates.ts
@@ -7,12 +7,16 @@ const maximum_wire_distance = settings.startup[
   `${settings_prefix}big-electric-pole:maximum-wire-distance`
 ].value as factorio.double;
 
-data.raw["electric-pole"]["big-electric-pole"].maximum_wire_distance =
-  maximum_wire_distance;
+if (data.raw["electric-pole"]["big-electric-pole"]) {
+  data.raw["electric-pole"]["big-electric-pole"].maximum_wire_distance =
+    maximum_wire_distance;
+}
 
 const supply_area_distace = settings.startup[
   `${settings_prefix}substation:supply-area-distance`
 ].value as factorio.double;
 
-data.raw["electric-pole"]["substation"].supply_area_distance =
-  supply_area_distace;
+if (data.raw["electric-pole"]["substation"]) {
+  data.raw["electric-pole"]["substation"].supply_area_distance =
+    supply_area_distace;
+}
